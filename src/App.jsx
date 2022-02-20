5dde1f00ff452d9186dee7809de54192d7f1f035
iva <Iva_Trifonova@epam.com>
2022-02-20 15:52:41 +0200

    createCourse

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
+import { CreateCourse } from './components/CreateCourse/CreateCourse';
 
 function App() {
 	return (
 		<div className='App'>
 			<Routes>
 				<Route path='/' element={<Home />} />
+				<Route exact path='/createCourse' element={<CreateCourse />} />
 			</Routes>
 		</div>
 	);
